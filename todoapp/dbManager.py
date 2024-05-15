6b0eef0f5e6fda54adcd0790a70502bd40820461
Cocolak <krystian.figiela000@gmail.com>
2024-05-15 19:25:04 +0200

    Updates

---
+++ todoapp/dbManager.py
@@ -0,0 +1,49 @@
+import sqlite3
+import os
+
+
+class DBManager:
+    def __init__(self, path):
+        self.db_path = path
+        self.check_if_db_exists()
+        # self.conn, self.cursor = self.get_conn_curs()
+
+    def check_if_db_exists(self):
+        if not os.path.exists(self.db_path):
+            file = open(self.db_path, 'w')
+            file.close()
+
+            conn, cursor = self.get_conn_cursor()
+            cursor.execute("CREATE TABLE tasks(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, isDone INTEGER)")
+            conn.close()
+
+    def get_conn_cursor(self):
+        conn = sqlite3.connect(self.db_path)
+        cursor = conn.cursor()
+        return conn, cursor
+
+    def get_tasks(self):
+        conn, cursor = self.get_conn_cursor()
+        query = 'SELECT name, isDone FROM tasks'
+
+        tasks = cursor.execute(query)
+        tasks = tasks.fetchall()
+        conn.close()
+        return tasks
+
+    def add_task(self, task):
+        conn, cursor = self.get_conn_cursor()
+        query = 'INSERT INTO tasks(name, isDone) VALUES(?, ?)'
+
+        cursor.execute(query, (task, 0))
+        conn.commit()
+        conn.close()
+
+    def checked_is_changed(self, task_name, new_value):
+        conn, cursor = self.get_conn_cursor()
+        query = 'UPDATE tasks SET isDone = ? WHERE name = ?'
+
+        cursor.execute(query, (new_value, task_name))
+        conn.commit()
+        conn.close()
+
--- todoapp/main.py
+++ todoapp/main.py
@@ -1,9 +1,11 @@
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton, QPlainTextEdit, QListWidget, QStackedWidget, \
-    QListWidgetItem
+    QListWidgetItem, QDesktopWidget
 from PyQt5 import uic
 import sys
+import sqlite3
 import res
+from dbManager import DBManager
 
 
 class TODOAPP(QMainWindow):
@@ -12,21 +14,60 @@ class TODOAPP(QMainWindow):
 
         uic.loadUi('design.ui', self)
         self.setWindowTitle('TODO APP')
+        self.setFixedSize(380, 520)
+        self.center()
+        self.stackedWidget.setCurrentIndex(0)
 
-        self.stacked_widget = self.findChild(QStackedWidget, "stackedWidget")
-        self.stacked_widget.setCurrentIndex(1)
+        self.dbManger = DBManager("database.db")
 
-        todos = ["Task Name", "Task Name", "Task Name", "Task Name", "Task Name", "Task Name", "Task Name"]
+        self.load_todos()
 
-        for todo in todos:
-            item = QListWidgetItem(todo)
-            item.setFlags(item.flags() | Qt.ItemIsUserCheckable)
-            item.setCheckState(Qt.Unchecked)
-            self.listWidget.addItem(item)
+        self.listWidget.itemClicked.connect(self.task_checked)
+        self.addTaskButton.clicked.connect(self.add_task_clicked)
+        self.confirmButton.clicked.connect(self.confirm_clicked)
+        self.cancelButton.clicked.connect(self.cancel_clicked)
 
         self.show()
 
+    def load_todos(self):
+        self.listWidget.clear()
+        tasks = self.dbManger.get_tasks()
 
-app = QApplication(sys.argv)
-UIWindow = TODOAPP()
-app.exec_()
+        for task in tasks:
+            item = QListWidgetItem(task[0])
+            item.setFlags(item.flags() | Qt.ItemIsUserCheckable)
+            if task[1] == 2:
+                item.setCheckState(Qt.Checked)
+            else:
+                item.setCheckState(Qt.Unchecked)
+            self.listWidget.addItem(item)
+
+    def task_checked(self, item):
+        self.dbManger.checked_is_changed(item.text(), item.checkState())
+
+    def add_task_clicked(self):
+        self.stackedWidget.setCurrentIndex(1)
+
+    def confirm_clicked(self):
+        # Print info that task is added
+        task_name = self.taskNameTextEdit.toPlainText()
+        self.dbManger.add_task(task_name)
+        self.taskNameTextEdit.clear()
+        self.stackedWidget.setCurrentIndex(0)
+        self.load_todos()
+
+    def cancel_clicked(self):
+        self.taskNameTextEdit.clear()
+        self.stackedWidget.setCurrentIndex(0)
+
+    def center(self):
+        qr = self.frameGeometry()
+        cp = QDesktopWidget().availableGeometry().center()
+        qr.moveCenter(cp)
+        self.move(qr.topLeft())
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    UIWindow = TODOAPP()
+    app.exec_()
